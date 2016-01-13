dafb0d6fb4f9a1a63979f7737a71c65076616f81
Johnathon Ludeman <jludeman@kabaminc.com>
2016-01-13 14:59:13 -0800

    add more to recipe

--- cookbooks/hello/recipes/default.rb
+++ cookbooks/hello/recipes/default.rb
@@ -7,4 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
+rightscale_marker
 log "Hello World! You should try Chef with RightScale!"
+
+package 'httpd'
+
+service 'httpd' do
+	action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
+end
+
+service 'iptables' do
+	action :stop
+end
