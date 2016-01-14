686ab6887900969e98b5258d12bb3df73c228b9c
Johnathon Ludeman <jludeman@kabaminc.com>
2016-01-14 13:53:29 -0800

    moved functionality to library for external use

---
+++ cookbooks/aws-tags/libraries/aws-tags-helper.rb
@@ -0,0 +1,15 @@
+module aws-tags
+	module helper
+
+		def service_tag(service)
+			log "Adding service tag '#{service}'..."
+			machine_tag "ec2:service=#{service}"
+		end
+
+		def deployment_tag(deployment)
+			log "Adding deployment tag '#{deployment}'..."
+			machine_tag "ec2:deployment=#{deployment}"
+		end
+
+	end
+end
--- cookbooks/aws-tags/recipes/base.rb
+++ cookbooks/aws-tags/recipes/base.rb
@@ -4,21 +4,11 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-def service_tag(service)
-    log "Adding service tag '#{service}'..."
-    machine_tag "ec2:service=#{service}"
+marker "recipe_start_rightscale" do
+	template "rightscale_audit_entry.erb"
 end
 
-def deployment_tag(deployment)
-    log "Adding deployment tag '#{deployment}'..."
-    machine_tag "ec2:deployment=#{deployment}"
-end
+::Chef::Recipe.send(:include, aws-tags::helper)
 
-if __FILE__ == $0
-	marker "recipe_start_rightscale" do
-		template "rightscale_audit_entry.erb"
-	end
-	
-	service_tag(node[:tagging][:service])
-	deployment_tag(node[:tagging][:deployment])
-end
+service_tag(node[:tagging][:service])
+deployment_tag(node[:tagging][:deployment])
