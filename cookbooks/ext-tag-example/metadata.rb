1c7d750dbe4cf4da3ed0457b4e57005d3d9e91e1
Johnathon Ludeman <jludeman@kabaminc.com>
2016-01-19 13:18:14 -0800

    renamed ext-tag-test to ext-tag-example

--- cookbooks/ext-tag-example/metadata.rb
+++ cookbooks/ext-tag-example/metadata.rb
@@ -1,11 +1,11 @@
-name             'ext-tag-test'
+name             'ext-tag-example'
 maintainer       'Kabam'
 maintainer_email 'jludeman@kabaminc.com'
 license          'All rights reserved'
-description      'Installs/Configures ext-tag-test'
+description      'Installs/Configures ext-tag-example'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'aws-tags'
 
-recipe 'ext-tag-test::default', 'test external use of tag library'
+recipe 'ext-tag-example::default', 'Example of external use of aws-tags library'
